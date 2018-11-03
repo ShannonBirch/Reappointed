ab4213c438c244d36e985c8e800ee2b2fb091bda
Shannon Birch <shannonbirch@live.ie>
2018-11-03 16:40:57 -0400

    Fooling around with google login
    Working on using a google login to enter the app instead of creating my own login.

--- Android/app/src/main/java/com/shannonbirch/appointments/LoginActivity.java
+++ Android/app/src/main/java/com/shannonbirch/appointments/LoginActivity.java
@@ -3,12 +3,21 @@ package com.shannonbirch.appointments;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private GoogleSignInClient  mGoogleSignInClient;
+    private static int RC_SIGN_IN = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,19 +27,56 @@ public class LoginActivity extends AppCompatActivity {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
-    }
 
-    @Override
-    public void OnClick(View v){
-        switch (v.getId()) {
-            case R.id.sign_in_button:
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        SignInButton google = findViewById(R.id.sign_in_button);
+        google.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.d("Here I am", "something something Hurricane");
                 signIn();
-                break;
-        }
+            }
+        });
     }
 
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e("Activity", "Ended");
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Log.d("Request Code", "Same");
+
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }else{
+            Log.e("Request Code", "Is different");
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Log.d("Signed In", account.toString());
+            // Signed in successfully, show authenticated UI.
+//            updateUI(account);
+        } catch (ApiException e) {
+            Log.e("Caught an error", e.toString());
+            //ToDo: Error happens here
+            // "com.google.android.gms.common.api.ApiException: 12500: "
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+//            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+//            updateUI(null);
+        }
     }
 }
